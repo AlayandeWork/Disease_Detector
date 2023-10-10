b38dfa3041ac0dcdb5f0545aff2a1686419f9a0e
Adeshina Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2023-10-10 14:13:05 +0100

    Added COVID and Parkinson Data to SQL

---
+++ SQL/Covid.sql
@@ -0,0 +1,13 @@
+CREATE TABLE covid (
+	id INT auto_increment,
+	Gender varchar(255),
+	Diarrhea varchar(255),
+	Fever varchar(255), 
+	Cough varchar(255), 
+	Headache varchar(255),
+	Sore_Throat varchar(255), 
+	Body_Ache varchar(255),
+	Vomiting varchar(255), 
+	Severity varchar(255),
+    PRIMARY KEY (id)
+);
